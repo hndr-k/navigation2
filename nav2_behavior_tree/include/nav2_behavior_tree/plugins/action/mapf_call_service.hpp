32c4b2c8328d82114ae804f169be1788eb7cb720
Hendrik <h.nessau@web.de>
2022-06-08 17:22:20 -0400

    adding service tree node for MAPF

---
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/mapf_call_service.hpp
@@ -0,0 +1,41 @@
+#include <memory>
+#include <string>
+
+#include "behaviortree_cpp_v3/action_node.h"
+#include "mapf_actions/srv/mapf.hpp"
+#include "nav2_behavior_tree/bt_conversions.hpp"
+#include "nav2_behavior_tree/bt_service_node.hpp"
+#include "nav2_util/geometry_utils.hpp"
+#include "nav2_util/node_utils.hpp"
+#include "nav2_util/robot_utils.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+namespace nav2_behavior_tree {
+class MapfCallService : public BtServiceNode<mapf_actions::srv::Mapf> {
+public:
+  MapfCallService(const std::string &service_node_name,
+                  const BT::NodeConfiguration &conf);
+
+  void on_tick() override;
+  BT::NodeStatus on_completion() override;
+  static BT::PortsList providedPorts() {
+    return providedBasicPorts({
+        BT::InputPort<int>("identifier",
+                           "idenftifier of robototino for MAPF request"),
+        BT::InputPort<geometry_msgs::msg::PoseStamped>(
+            "goal", "Destination to plan to"),
+        BT::InputPort<std::vector<geometry_msgs::msg::PoseStamped>>(
+            "mapf_goals", "Destinations to plan through"),
+        BT::InputPort<std::string>("global_frame", std::string("map"),
+                                   "Global frame"),
+        BT::InputPort<std::string>("robot_base_frame", std::string("base_link"),
+                                   "Robot base frame"),
+
+    });
+  }
+
+  std::string robot_base_frame_, global_frame_;
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  geometry_msgs::msg::PoseStamped goal_pose_;
+};
+} // namespace nav2_behavior_tree
---
+++ nav2_behavior_tree/plugins/action/mapf_call_service.cpp
@@ -0,0 +1,38 @@
+#include "nav2_behavior_tree/plugins/action/mapf_call_service.hpp"
+#include <memory>
+#include <string>
+
+namespace nav2_behavior_tree {
+MapfCallService::MapfCallService(const std::string &service_node_name,
+                                 const BT::NodeConfiguration &conf)
+    : BtServiceNode<mapf_actions::srv::Mapf>(service_node_name, conf) {
+      RCLCPP_INFO(rclcpp::get_logger("MAPF"),
+                "MAPF call instantiated");
+    }
+
+void MapfCallService::on_tick() {
+  getInput("identifier", request_->robotino_id);
+  getInput("goal", request_->goal);
+  getInput("global_frame", global_frame_);
+  getInput("robot_base_frame", robot_base_frame_);
+  tf_ = config().blackboard->get<std::shared_ptr<tf2_ros::Buffer>>("tf_buffer");
+  auto node = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+
+  geometry_msgs::msg::PoseStamped current_pose;
+  if (!nav2_util::getCurrentPose(current_pose, *tf_, global_frame_,
+                                 robot_base_frame_, 0.2)) {
+    RCLCPP_INFO(node->get_logger(),
+                "MAPF call unsuccesfull no localization possible");
+  }
+  request_->start = current_pose;
+}
+BT::NodeStatus MapfCallService::on_completion() {
+  setOutput("goals", future_result_.get()->path.poses);
+  return BT::NodeStatus::SUCCESS;
+}
+} // namespace nav2_behavior_tree
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory) {
+  factory.registerNodeType<nav2_behavior_tree::MapfCallService>(
+      "MapfCallService");
+}
